b83bc5672d6f1e1cc18f99ff52ef4029867f5ef1
j- <j@skeoh.com>
2026-04-06 15:29:12 +1000

    Add getUserMedia actions

--- src/routes/navigator[.]mediaDevices.tsx
+++ src/routes/navigator[.]mediaDevices.tsx
@@ -14,6 +14,40 @@ function RouteComponent() {
       heading="navigator.mediaDevices"
       name="navigator.mediaDevices"
       initialValue={() => navigator.mediaDevices}
+      actions={[
+        {
+          buttonProps: {
+            children: 'navigator.mediaDevices.getUserMedia()',
+          },
+          initialData: () => (
+            navigator.mediaDevices.getUserMedia()
+          ),
+        },
+        {
+          buttonProps: {
+            children: 'navigator.mediaDevices.getUserMedia({ audio: true })',
+          },
+          initialData: () => (
+            navigator.mediaDevices.getUserMedia({ audio: true })
+          ),
+        },
+        {
+          buttonProps: {
+            children: 'navigator.mediaDevices.getUserMedia({ video: true })',
+          },
+          initialData: () => (
+            navigator.mediaDevices.getUserMedia({ video: true })
+          ),
+        },
+        {
+          buttonProps: {
+            children: 'navigator.mediaDevices.getUserMedia({ audio: true, video: true })',
+          },
+          initialData: () => (
+            navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+          ),
+        },
+      ]}
     />
   );
 }
