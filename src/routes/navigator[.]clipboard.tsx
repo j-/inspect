f9e3702f3c7500eb54540fe6bd311511a6307cf8
j- <j@skeoh.com>
2026-04-06 20:11:38 +1000

    Output clipboard-read permission query

--- src/routes/navigator[.]clipboard.tsx
+++ src/routes/navigator[.]clipboard.tsx
@@ -1,3 +1,4 @@
+import Stack from '@mui/material/Stack';
 import { createFileRoute, useLocation } from '@tanstack/react-router';
 import { ObjectViewerPanel } from '#/components/ObjectViewerPanel';
 
@@ -9,29 +10,41 @@ function RouteComponent() {
   const { pathname } = useLocation();
 
   return (
-    <ObjectViewerPanel
-      id={pathname}
-      heading="navigator.clipboard"
-      name="navigator.clipboard"
-      initialValue={() => navigator.clipboard}
-      actions={[
-        {
-          buttonProps: {
-            children: 'navigator.clipboard.read()',
+    <Stack gap={4}>
+      <ObjectViewerPanel
+        id={pathname + '#queryPermission'}
+        heading="clipboard-read permission"
+        initialValue={() => (
+          navigator.permissions.query({
+            name: 'clipboard-read' as PermissionName,
+          })
+        )}
+      />
+
+      <ObjectViewerPanel
+        id={pathname}
+        heading="navigator.clipboard"
+        name="navigator.clipboard"
+        initialValue={() => navigator.clipboard}
+        actions={[
+          {
+            buttonProps: {
+              children: 'navigator.clipboard.read()',
+            },
+            initialData: () => (
+              navigator.clipboard.read()
+            ),
           },
-          initialData: () => (
-            navigator.clipboard.read()
-          ),
-        },
-        {
-          buttonProps: {
-            children: 'navigator.clipboard.readText()',
+          {
+            buttonProps: {
+              children: 'navigator.clipboard.readText()',
+            },
+            initialData: () => (
+              navigator.clipboard.readText()
+            ),
           },
-          initialData: () => (
-            navigator.clipboard.readText()
-          ),
-        },
-      ]}
-    />
+        ]}
+      />
+    </Stack>
   );
 }
