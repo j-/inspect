c5dea1ea2cfbdbb975c9801a3f90532d2573ac61
j- <j@skeoh.com>
2026-07-10 18:48:28 +1000

    Fix layout map route on Firefox

--- src/routes/navigator[.]keyboard[.]getLayoutMap().tsx
+++ src/routes/navigator[.]keyboard[.]getLayoutMap().tsx
@@ -1,7 +1,5 @@
 import Stack from '@mui/material/Stack';
 import { createFileRoute, useLocation } from '@tanstack/react-router';
-import { createIsomorphicFn } from '@tanstack/react-start';
-import { useEffect, useMemo, useState } from 'react';
 import { ObjectViewerPanel } from '#/components/ObjectViewerPanel';
 import { eager } from '#/resource';
 
@@ -9,48 +7,27 @@ export const Route = createFileRoute('/navigator.keyboard.getLayoutMap()')({
   component: RouteComponent,
 });
 
-const initLayoutMap = createIsomorphicFn()
-  .client(() => navigator.keyboard.getLayoutMap())
-  .server(() => null);
-
 function RouteComponent() {
   const { pathname } = useLocation();
-
-  const promise = useMemo(() => {
-    return initLayoutMap();
-  }, []);
-
-  const [entries, setEntries] = useState<[KeyMapCode, string][] | null>(null);
-
-  useEffect(() => {
-    let isMounted = true;
-
-    promise?.then((layoutMap: Map<KeyMapCode, string>) => {
-      if (!isMounted) return;
-
-      setEntries(Array.from(layoutMap.entries()));
-    });
-
-    return () => {
-      isMounted = false;
-    };
-  }, [promise]);
 
   return (
     <Stack gap={4}>
       <ObjectViewerPanel
         id={pathname + '#layoutMap'}
         heading="navigator.keyboard.getLayoutMap()"
-        resource={eager(() => promise)}
+        resource={eager(() => navigator.keyboard.getLayoutMap())}
       />
 
-      {entries && (
-        <ObjectViewerPanel
-          id={pathname + '#layoutMapEntries'}
-          heading="Layout Map Entries"
-          resource={eager(() => entries)}
-        />
-      )}
+      <ObjectViewerPanel
+        id={pathname + '#layoutMapEntries'}
+        heading="Layout Map Entries"
+        resource={eager(() => {
+          const promise = navigator.keyboard.getLayoutMap();
+          return promise.then((layoutMap) => {
+            return Array.from(layoutMap.entries());
+          });
+        })}
+      />
     </Stack>
   );
 }
